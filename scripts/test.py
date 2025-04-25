1bb78452059f3b3a901519efe3eff4c64f256374
ntnhtc <han.ntnhtc@gmail.com>
2025-04-26 00:24:18 +0700

    config spark session successfully

---
+++ scripts/test.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+import os
+
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY_ID") ## AWS CREDENTIALS
+AWS_SECRET_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY") ## AWS CREDENTIALS
+
+spark = (
+    SparkSession.builder
+    .appName("test")
+    .config("spark.hadoop.fs.s3a.access.key", AWS_ACCESS_KEY)
+    .config("spark.hadoop.fs.s3a.secret.key", AWS_SECRET_KEY)
+    .getOrCreate()
+)
+
+print("Spark Running")
+
+for item in spark.sparkContext.getConf().getAll():
+    print(item)
+
+# spark.sql("CREATE TABLE nessie.names (name STRING) USING iceberg;").show()
+
+# spark.sql("INSERT INTO nessie.names VALUES ('Alex Merced'), ('Dipankar Mazumdar'), ('Han Nguyen')").show()
+
+# spark.sql("SELECT * FROM nessie.names;").show()
